304b23003ba387c9da0fdffb5e1b4a66beb29085
ashi8895 <ashigupta318@gmail.com>
2025-05-19 13:50:08 +0000

    datatype

---
+++ .devcontainer/02_dataType.js
@@ -0,0 +1,18 @@
+console.log(3+3) // code readability should be high
+console.log("Ashi")
+
+let name = "ashi"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof "ashi"); // undefined
+console.log(typeof "null"); //object 
